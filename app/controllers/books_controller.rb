c709d7b4b5e2ffc5b2213138462c984feacd7929
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-16 00:23:15 +0000

    [Finish] 応用課題8b

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,7 +50,7 @@ class BooksController < ApplicationController
 
 
   def is_matching_login_user
-    book = Book.find(params[:id])
+    book = Book.find(params[:id, :user_id])
     unless book.user_id == current_user.id
       redirect_to books_path
     end
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,28 +1,44 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :reject_non_related, only: [:show]
+
   def show
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
-    unless user_rooms.nil?
-      @room = user_rooms.room
+    user_room = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    if user_room.present?
+      @room = user_room.room
     else
-      @room = Room.new
-      @room.save
+      @room = Room.create
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
+
   def create
     @chat = current_user.chats.new(chat_params)
     @room = @chat.room
     @chats = @room.chats
-    render :validater unless @chat.save
+    if @chat.save
+      redirect_to request.referer
+    else
+      render 'show'
+    end
   end
 
   private
+  def reject_non_related
+    user = User.find(params[:id])
+    unless current_user.following?(user) && user.following?(current_user)
+      redirect_to books_path
+    end
+  end
+
   def chat_params
     params.require(:chat).permit(:message, :room_id)
   end
+
+
 end
